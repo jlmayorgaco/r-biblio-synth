f6fe933a98cd3daddda41dab0b49780fe1119113
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-08-13 09:57:01 -0500

    Update main.r

--- examples/SCOPUS_POWER_SYSTEMS_FREQUENCY_ESTIMATOR/main.r
+++ examples/SCOPUS_POWER_SYSTEMS_FREQUENCY_ESTIMATOR/main.r
@@ -3,16 +3,16 @@
 # --------------------------------------------------- #
 
 # Source the SystematicReview class definition
-source('libs/RBiblioMetricsAnalyzer/SystematicReviewClass.r')
+source('../../src/SystematicReviewClass.r')
 
 # SystematicReview Class
 systematicReview <- SystematicReview$new()
 
 # Add Config Settings
-systematicReview$setDate("Wednesday, July 10, 2024 1:50:56 AM")
+systematicReview$setBibPath("data/scopus.bib")
 systematicReview$setTitle("Power Systems Frequency Estimators from 1960 to 2023")
+systematicReview$setDate("Wednesday, July 10, 2024 1:50:56 AM")
 systematicReview$setQuery("TITLE-ABS-KEY ( power AND system AND frequency AND estimator ) from 1960 to 2023")
-systematicReview$setBibPath("data/scopus/query_power_systems_frequency_estimator_from_1960_to_2023/scopus.bib")
 systematicReview$setKeywords(c("power", "system", "frequency", "estimator"))
 
 # Load and Init Data
@@ -21,9 +21,9 @@ systematicReview$init()
 # Check Status and Requirements
 systematicReview$do_m0_check_health_status()
 systematicReview$do_m0_check_required_columns()
+systematicReview$do_m0_cleaning_data()
 
 # Modules
-systematicReview$do_m1_cleaning()
 systematicReview$do_m1_main_information()
 systematicReview$do_m2_author_prod_over_time_regression()
 # systematicReview$do_m3_authors()
