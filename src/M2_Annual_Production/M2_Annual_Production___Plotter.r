bcbe0d02ca21e2b081f2e6a80ba6a296d3dcf0aa
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-08-14 09:34:39 -0500

    Update M2_Annual_Production___Plotter.r

--- src/M2_Annual_Production/M2_Annual_Production___Plotter.r
+++ src/M2_Annual_Production/M2_Annual_Production___Plotter.r
@@ -2,16 +2,7 @@
 library(ggplot2)
 library(ggsci)
 
-# Define a color palette
-plotters_colors <- c(
-  "Blue" = "#3785cf",
-  "Green" = "#46a152",
-  "Orange" = "#e85803",
-  "Yellow" = "#ffc103",
-  "Brown" = "#8f2802",
-  "DarkBlue" = "#0a2967",
-  "Darkgrey" = "#232323"
-)
+
 
 # Function to add a valid line to the plot
 add_line_if_valid <- function(plot, data, color, linetype, model_name, best_model) {
@@ -41,23 +32,7 @@ save_moving_average_plots <- function(df_title, df_name, year_column, df, moving
   dir_path <- "results/M2_Annual_Production"
   ensure_directory(dir_path)
 
-  # Define IEEE style theme
-  ieee_theme <- theme(
-    text = element_text(size = 12, family = "Times New Roman"),
-    legend.title = element_text(size = 10),
-    legend.text = element_text(size = 8),
-    axis.title = element_text(size = 12),
-    axis.line = element_line(color = "black", size = 0.25, linetype = "solid"),
-    axis.text = element_text(size = 10),
-    panel.background = element_rect(fill = "white"),
-    panel.grid.major = element_line(color = "gray95", size = 0.25),
-    panel.grid.minor = element_line(color = "gray95", size = 0.125),
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.5),
-    plot.margin = margin(t = 5, r = 5, b = 5, l = 5),
-    axis.ticks = element_line(color = "black"),
-    axis.ticks.length = unit(0.2, "cm"),
-    axis.ticks.length.minor = unit(0.1, "cm")
-  )
+  
 
   # Plot all moving averages together
   p_all <- ggplot(df, aes_string(x = year_column)) +
@@ -101,36 +76,114 @@ save_moving_average_plots <- function(df_title, df_name, year_column, df, moving
 
 # Function to create the plot
 create_moving_average_plot <- function(data, output_path) {
-  ieee_theme <- theme(
-    text = element_text(size = 12, family = "Times New Roman"),
-    legend.title = element_text(size = 10),
-    legend.text = element_text(size = 8),
-    axis.title = element_text(size = 12),
-    axis.line = element_line(color = "black", size = 0.25, linetype = "solid"),
-    axis.text = element_text(size = 10),
-    panel.background = element_rect(fill = "white"),
-    panel.grid.major = element_line(color = "gray95", size = 0.25),
-    panel.grid.minor = element_line(color = "gray95", size = 0.125),
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.5),  # Center title
-    plot.margin = margin(t = 5, r = 5, b = 5, l = 5),
-    axis.ticks = element_line(color = "black"),
-    axis.ticks.length = unit(0.2, "cm"),
-    axis.ticks.length.minor = unit(0.1, "cm")
-  )
 
-  plot <- ggplot(data, aes(x = Year, y = Articles, color = Type)) +
-    geom_point(size = 2) +
-    geom_line(size = 0.75) +
-    labs(
-      title = "Moving Averages Over the Years",
-      x = "Year",
-      y = "Moving Average of Articles",
-      color = "Type"
-    ) +
-    ieee_theme +
-    scale_color_manual(values = c("red", "blue", "green")) +
-    theme_minimal() +
-    theme(legend.position = "bottom")
 
-  ggsave(filename = output_path, plot = plot, width = 10, height = 6, dpi = 300)
+    df <- data
+    year_column <- 'Year'
+    df_column <- 'Articles'
+
+    # Calculate minimum and maximum values
+    MinX <- min(df[[year_column]], na.rm = TRUE)
+    MinY <- min(df[[df_column]], na.rm = TRUE)
+    MaxX <- max(df[[year_column]], na.rm = TRUE)
+    MaxY <- max(df[[df_column]], na.rm = TRUE)
+
+    # Calculate break intervals
+    Nx <- 5
+    Ny <- 6
+    DX <- MaxX - MinX
+    DY <- MaxY - MinY
+
+    # Ensure nx and ny are valid
+    nx <- max(1, ceiling(DX / Nx), na.rm = TRUE)  # Ensure nx is at least 1
+    ny <- max(1, ceiling(DY / Ny), na.rm = TRUE)  # Ensure ny is at least 1
+
+    # Calculate breaks with a check to prevent invalid sequences
+    x_breaks <- seq(MinX, MaxX, by = nx)
+    x_breaks <- ceiling(x_breaks[x_breaks <= MaxX])  # Ensure breaks do not exceed max value
+    y_breaks <- seq(MinY, MaxY, by = ny)
+    y_breaks <- ceiling(y_breaks[y_breaks <= MaxY])  # Ensure breaks do not exceed max value
+
+    v_labs <- labs(
+        title = "Moving Averages Over the Years",
+        x = "Year",
+        y = "Moving Average of Articles",
+        color = "Type"
+    )
+
+    color1 <- THEME_COLORS["Yellow"]
+    colors <- c("#8f2802", "#232323",  "#cfcfcf")
+
+    # Plotting the growth rate over the years
+    p <- ggplot(data, aes(x = Year, y = Articles, color = Type))
+    p <- p + geom_line( size = 0.15, linetype = "dashed")
+    p <- p + geom_point( size = 1, shape = 20)
+    p <- p + theme_minimal(base_size = 10)
+    p <- p + v_labs
+    p <- p + ieee_theme 
+    p <- p + scale_color_manual(values = colors)
+    p <- p + theme(legend.position = "bottom")
+    #p <- p + theme_publish()
+
+    p <- p + scale_y_continuous(
+      expand = c(0, 0),
+      limits = c(MinY, MaxY),
+      breaks = y_breaks
+      #minor_breaks = seq(min(y_breaks), max(y_breaks), by = ny / 10)  # Adding minor breaks
+    )
+    p <- p + scale_x_continuous(
+      expand = c(0, 0),
+      limits = c(MinX, MaxX),
+      breaks = x_breaks
+      #minor_breaks = seq(min(x_breaks), max(x_breaks), by = nx / 10)  # Adding minor breaks
+    )
+   
+
+  ggsave(filename = output_path, plot = p, width = 3.5, height = 2.5, dpi = 900)
+}
+
+
+# Function to save individual plots
+save_individual_plots <- function(data, output_dir) {
+  
+  # Ensure the output directory exists
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+  }
+  
+  # Extract unique types from the data
+  types <- unique(data$Type)
+  
+  # Loop through each type to create and save individual plots
+  for (type in types) {
+    # Filter data for the current type
+    df_type <- subset(data, Type == type)
+    
+    # Define the output file paths
+    output_file_png <- file.path(output_dir, paste0("Plot_", gsub(" ", "_", type), ".png"))
+    output_file_svg <- file.path(output_dir, paste0("Plot_", gsub(" ", "_", type), ".svg"))
+    
+    # Create the plot
+    p <- ggplot(df_type, aes(x = Year, y = Articles, color = Type)) +
+      geom_line(size = 0.8, linetype = "solid") +
+      geom_point(size = 1.5, shape = 20) +
+      theme_minimal(base_size = 10) +
+      labs(
+        title = paste(type, "Over the Years"),
+        x = "Year",
+        y = "Moving Average of Articles",
+        color = "Type"
+      ) +
+      ieee_theme +
+      scale_color_manual(values = c(THEME_COLORS[type])) +
+      theme(legend.position = "bottom") +
+      scale_y_continuous(expand = c(0, 0), limits = range(df_type$Articles, na.rm = TRUE)) +
+      scale_x_continuous(expand = c(0, 0), limits = range(df_type$Year, na.rm = TRUE))
+    
+    # Save the plot as PNG
+    ggsave(filename = output_file_png, plot = p, width = 3.5, height = 2.5, dpi = 900)
+    
+    # Save the plot as SVG
+    ggsave(filename = output_file_svg, plot = p, width = 3.5, height = 2.5, device = "svg")
+  }
 }
