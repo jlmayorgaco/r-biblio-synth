f515b44a4036e9bdfe5eca5109b0349ca4fc98c0
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-03-16 21:42:42 -0500

    Create _plots.r

---
+++ src/M2_Annual_Production/plots/_plots.r
@@ -0,0 +1,8 @@
+
+source('../../src/M2_Annual_Production/plots/Helpers.r')
+source('../../src/M2_Annual_Production/plots/R_IEEE_Plot.r')
+source('../../src/M2_Annual_Production/plots/Regression_Model_Gompertz_Plot.r')
+
+
+source('../../src/M2_Annual_Production/plots/Plots_m2_m1_regression_plots.r')
+source('../../src/M2_Annual_Production/plots/Plots_m2_m2_harmonics_plots.r')
