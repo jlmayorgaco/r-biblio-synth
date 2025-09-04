9ec1cc79a408ce21eed97306843b638799b50caa
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-09-04 17:11:31 -0500

    Create colors.r

---
+++ src2/themes/ieee/colors.r
@@ -0,0 +1,50 @@
+# ---------------------------------------------------------------------------- #
+# THEME_COLORS Palette
+# ---------------------------------------------------------------------------- #
+IEEE_MAYORGA_THEME_COLORS <- list(
+  Main = c("#4e79a7", "#f28e2b", "#e15759", "#76b7b2", "#59a14f", "#edc949"),
+  Grayscale = list(
+    Black = "#000000",
+    VeryDarkGray = "#2b2b2b",
+    DarkGray = "#4d4d4d",
+    MediumGray = "#808080",
+    LightGray = "#d3d3d3",
+    VeryLightGray = "#f2f2f2",
+    White = "#ffffff"
+  ),
+  Categorical = c(
+    Lower = "#e15759",
+    Medium = "#f28e2b",
+    Bigger = "#4e79a7"
+  ),
+  Sequential = c(
+    VeryLight = "#deebf7",
+    Light = "#9ecae1",
+    Medium = "#3182bd",
+    Dark = "#08306b"
+  ),
+  Diverging = c(
+    VeryLow = "#d73027",
+    Low = "#fc8d59",
+    Neutral = "#fee08b",
+    High = "#91bfdb",
+    VeryHigh = "#4575b4"
+  ),
+  Highlight = c(
+    Positive = "#1b9e77",
+    Negative = "#d95f02",
+    Neutral = "#7570b3"
+  ),
+  Text = list(
+    Title = "#333333",
+    Subtitle = "#555555",
+    Caption = "#888888",
+    Axis = "#444444",
+    Highlight = "#1b9e77"
+  ),
+  Background = list(
+    Light = "#ffffff",
+    Medium = "#f4f4f4",
+    Dark = "#2b2b2b"
+  )
+)
