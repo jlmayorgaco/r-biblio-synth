c988354ee5808e1bde02978ed5899a4e0b842d6f
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-09-08 07:29:17 -0500

    Update exponential_model.r

--- src2/stats/models/exponential_model.r
+++ src2/stats/models/exponential_model.r
@@ -31,6 +31,7 @@ ExponentialModel <- R6::R6Class(
         type   = "exponential",
         formula = deparse(formula(self$model)),
         coef    = as.list(coef(self$model)),
+        Robustness = robust_for_json(self$robust),
         stats   = list(
           rss  = tryCatch(deviance(self$model), error = function(e) NA_real_),
           R2   = self$r2(),
