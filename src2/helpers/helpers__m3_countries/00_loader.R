8569ca0789944a22b77c7d24b559d820d4fa7811
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-10-15 11:26:21 -0500

    Delete 00_loader.R

--- src2/helpers/helpers__m3_countries/00_loader.R
+++
@@ -1,8 +0,0 @@
-# Loads all helper files in deterministic order
-m3c_source_helpers <- function(dir = "src2/helpers/helpers__m3_countries") {
-  stopifnot(dir.exists(dir))
-  files <- list.files(dir, pattern = "^[0-9]{2}_.+\\.R$", full.names = TRUE)
-  files <- files[order(basename(files))]
-  for (f in files) sys.source(f, envir = parent.frame())
-  invisible(TRUE)
-}
