bb6e23d9e20f339838aceaa4b8e2aa6512cffa44
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-11-02 20:58:47 -0500

    Delete _plots.r

--- src/M4_Countries/plots/_plots.r
+++
@@ -1,5 +0,0 @@
-
-source('../../src/M4_Countries/plots/Helpers.r')
-source('../../src/M4_Countries/plots/R_IEEE_Plot.r')
-source('../../src/M4_Countries/plots/Plot_TopBottom10_Countries.r')
-source('../../src/M4_Countries/plots/Map_Countries_Quadrant_Plot.r')
